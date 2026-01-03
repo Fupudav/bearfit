a909ad7b2a16c8a0bd5586ebb88a07e60ba1b566
Fupudav <rlopez.perso@gmail.com>
2026-01-03 18:16:38 +0100

    Fix safeClone recursion

--- storage.js
+++ storage.js
@@ -38,7 +38,7 @@ const DAY_KEYS = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
 function safeClone(value) {
   if (typeof structuredClone === "function") {
-    return safeClone(value);
+    return structuredClone(value);
   }
   return JSON.parse(JSON.stringify(value));
 }
